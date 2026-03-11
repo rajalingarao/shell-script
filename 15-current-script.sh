fb16539dee90090a1a327d3638fa42dc4e6e1864
lingaiah-ctu <rajalingarao4545@gmail.com>
2026-03-11 16:34:23 +0530

    shell script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -1,10 +1,16 @@
+# #!/bin/bash
+# COURSE="DevOps from Current Script"
+# echo "Before calling other script, course: $COURSE"
+# echo "Process ID of current shell script: $$"
+
+# ./16-other-script.sh
+# echo "After calling other script, course: $COURSE"
+
+
 #!/bin/bash
-
 COURSE="DevOps from Current Script"
-
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./16-other-script.sh
-
+source /16-other-script.sh
 echo "After calling other script, course: $COURSE"
--- 16-other-script.sh
+++ 16-other-script.sh
@@ -4,3 +4,6 @@ COURSE="DevOps from Other Script"
 
 echo "Variable value from Other script: $COURSE"
 echo "Process ID of other script: $$"
+
+
+# chmod +x 16-other-script.sh
