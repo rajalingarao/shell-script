52e80acb8f1a96537c8fb73c88afe37bae0ad0b9
lingaiah-ctu <rajalingarao4545@gmail.com>
2026-03-11 16:57:44 +0530

    changes to shell script

---
+++ 17-set.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+failure(){
+    echo "Failed at $1:$2"
+}
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u) #ERR
+if [ $USERID -ne 0 ]
+then
+   echo "Please run this script with root access."
+   exit 1
+else
+   echo "You are super user."
+fi
+
+dnf install mysqldff -y
+dnf install git -y
+
+echo "is script proceeding?"
