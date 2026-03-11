e069e817dde36a629cb70b47cb70afaca957e484
lingaiah-ctu <rajalingarao4545@gmail.com>
2026-03-11 16:35:26 +0530

    shell script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -12,5 +12,5 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-source /16-other-script.sh
+source ./16-other-script.sh
 echo "After calling other script, course: $COURSE"
